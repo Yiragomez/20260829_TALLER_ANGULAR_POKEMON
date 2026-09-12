92797266ecfea9ec233b641dad5cfb6b41261eff
Yiragomez2 <3143857707yira@gmail.com>
2026-09-12 15:28:10 +0000

    Cambios

--- BuscadorPokemon/src/app/app.routes.ts
+++ BuscadorPokemon/src/app/app.routes.ts
@@ -2,10 +2,17 @@ import { Routes } from '@angular/router';
 import { RegistroUsuario } from './components/registro-usuario/registro-usuario';
 import { BuscadorPokemon } from './components/buscador-pokemon/buscador-pokemon';
 import { InventarioPokemon } from './components/inventario-pokemon/inventario-pokemon';
+import { Evaluacion3275760 } from './components/evaluacion/evaluacion.3275760';
+import { PruebaPrueba } from './components/prueba/prueba.prueba';
+import { Formulario } from './formulario/formulario';
 
 export const routes: Routes = [
     { path: 'registro', component: RegistroUsuario },
     { path: 'buscador', component: BuscadorPokemon },
     { path: 'inventario', component: InventarioPokemon },
-    { path: '', redirectTo: '/buscador', pathMatch: 'full' }
+    { path: 'formulario', component: Formulario },
+    { path: 'evaluacion', component: Evaluacion3275760 },
+    { path: 'prueba', component: PruebaPrueba },
+    { path: '', redirectTo: '/buscador', pathMatch: 'full' },
+    { path: '**', redirectTo: '/buscador' },
 ];
